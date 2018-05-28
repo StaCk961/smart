fdd9236391cdf121c9bae35675a05a105545b0f0
Iskandar Salama <iskandarsalama@Iskandars-MacBook-Pro.local>
2018-05-28 21:42:54 +0300

    after hackathon

---
+++ Coursetro.sol
@@ -0,0 +1,28 @@
+pragma solidity ^0.4.18;
+
+contract Coursetro {
+    
+   string fName;
+   uint age;
+   
+   
+    event Instructor(
+       string name,
+       uint age
+    );
+    
+   function setInstructor(string _fName, uint _age) public {
+       fName = _fName;
+       age = _age;
+       
+        Instructor(_fName, _age);
+        
+   }
+   
+   function getInstructor() public constant returns (string, uint) {
+       return (fName, age);
+   }
+    
+}
+
+
